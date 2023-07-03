28276e7cffe8b50fb6adb09b33f7ccae0b5e7088
Joe Burgess <joeburgess@tds.net>
2023-07-03 12:48:59 -0400

    Setting base

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,37 @@
 @extends('layouts.no-sidebar')
 
-@section('title', 'Page Title')
+@section('title', 'ReelzList')
 
 @section('content')
-    <p>This is my body content.</p>
+
+<div class="row">
+    <p>If you're here then you know.  I do need to give a huge shout out to <a href="https://www.omdbapi.com">OMDb</a> and <a href="https://www.themoviedb.org">TMDb</a> and JustWatch</p>
+    <p>I'm making this with bootstrap <a href="https://bootswatch.com/lux/">Lux</a> from Bootswatch.
+</div>
+<div class="row">
+    <p>This is where I'm going to keep a list of features to be built out</p>
+    <ul>
+        <li>Users</li>
+        <li>User Profile</li>
+        <li><ul>Users List
+                <li>Favorite</li>
+                <li>Ownership</li>
+                <li>Wish List to see</li>
+                <li>Wish List to own</li>
+            </ul>
+        </li>
+        <li>Users Reviews</li>
+        <li>Where to watch</li>
+    </ul>
+</div>
+<div class="row">
+    <p>In the making of this App I watched:
+        <ol>
+            <li>Mozart and the Whale</li>
+            <li>Megan</li>
+            <li>Chicken Run</li>
+        </ol>
+    </p>
+</div>
+
 @endsection
--- resources/views/layouts/no-sidebar.blade.php
+++ resources/views/layouts/no-sidebar.blade.php
@@ -8,13 +8,35 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
         <title>@yield('title')</title>
     </head>
     <body>
+        
+    <!-- Navigation Bar -->
+    <div class="navbar navbar-expand-lg fixed-top bg-primary" data-bs-theme="dark">
         <div class="container">
-            @yield('content')
+            <a class="navbar-brand" href="/">Home</a>
+            <div id="navbarResponsive" class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../search/">Movie Search</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../help/">Help</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://blog.bootswatch.com/">Blog</a>
+                    </li>
+                </ul>
+            </div>
         </div>
+    </div>
+
+    <!-- Content Section -->
+    <div class="container">
+        @yield('content')
+    </div>
     </body>
 </html>
